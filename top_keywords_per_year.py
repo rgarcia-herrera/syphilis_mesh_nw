5211f1dff43de841a7dd27b31beec32f35bc1bf7
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-09-27 00:55:02 -0500

    top 10 keywords per year

---
+++ top_keywords_per_year.py
@@ -0,0 +1,47 @@
+import argparse
+from pattern.vector import Document
+from Bio import Medline
+import time, datetime
+from itertools import combinations
+from pprint import pprint
+import networkx as nx
+import sys
+
+parser = argparse.ArgumentParser(description='grab terms from medline')
+parser.add_argument('medline', type=argparse.FileType('r'), default=sys.stdin, help="citations file in medline format")
+
+args    = parser.parse_args()
+
+records = Medline.parse( args.medline )
+
+g = nx.Graph()
+
+terms = {n:[] for n in range(1817,2017)}
+
+for r in records:
+
+    # evenly format dates
+    if 'EDAT' in r.keys():
+        try:
+            conv = time.strptime( r['EDAT'], "%Y/%m/%d %H:%M" )
+            date = datetime.datetime(*conv[:6]) # entrez date
+        except ValueError:
+            conv = time.strptime( r['EDAT'], "%Y/%m/%d" )
+            date = datetime.datetime(*conv[:6]) # entrez date
+
+        if 'MH' in r:
+            pass
+        elif 'OT' in r:
+            pass
+        elif 'TI' in r:
+            d = Document(r['TI'])
+            terms[date.year]+=[w[1] for w in d.keywords()]
+        else:
+            d = Document(r['AB'])
+            terms[date.year]+=[w[1] for w in d.keywords()]
+
+
+for year in terms:
+    d=Document(terms[year])
+    print year,[w[1] for w in d.keywords(top=10)]
+
