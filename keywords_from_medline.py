40806ec28abece76b2c7fb1f8119c87b9864e1a2
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-09-27 00:56:05 -0500

    keywords per article

--- keywords_from_medline.py
+++ keywords_from_medline.py
@@ -1,4 +1,5 @@
 import argparse
+from pattern.vector import Document
 from Bio import Medline
 import time, datetime
 from itertools import combinations
@@ -6,7 +7,7 @@ from pprint import pprint
 import networkx as nx
 import sys
 
-parser = argparse.ArgumentParser(description='create a meshterm network from a medline file')
+parser = argparse.ArgumentParser(description='grab terms from medline')
 parser.add_argument('medline', type=argparse.FileType('r'), default=sys.stdin, help="citations file in medline format")
 
 args    = parser.parse_args()
@@ -18,33 +19,22 @@ g = nx.Graph()
 for r in records:
     
     # evenly format dates
-    if 'CRDT' in r.keys():
-        conv = time.strptime( r['CRDT'][0], "%Y/%m/%d %H:%M" )
-        r['CRDT'] = datetime.datetime(*conv[:6]) # date created
-    if 'DCOM' in r.keys():
-        conv = time.strptime( r['DCOM'], "%Y%m%d" )
-        r['DCOM'] = datetime.datetime(*conv[:6]) # date completed
-    if 'LR' in r.keys():
-        conv = time.strptime( r['LR'], "%Y%m%d" )
-        r['LR'] = datetime.datetime(*conv[:6]) # date revised
-    if 'DEP' in r.keys():
-        conv = time.strptime( r['DEP'], "%Y%m%d" )
-        r['DEP'] = datetime.datetime(*conv[:6]) # date of electronic publication
+    if 'EDAT' in r.keys():
+        try:
+            conv = time.strptime( r['EDAT'], "%Y/%m/%d %H:%M" )
+            r['EDAT'] = datetime.datetime(*conv[:6]) # entrez date
+        except ValueError:
+            conv = time.strptime( r['EDAT'], "%Y/%m/%d" )
+            r['EDAT'] = datetime.datetime(*conv[:6]) # entrez date
+            
+        if 'MH' in r:
+            pass
+        elif 'OT' in r:
+            pass
+        elif 'TI' in r:
+            d = Document(r['TI'])
+            print r['EDAT'], [w[1] for w in d.keywords()]
+        else:
+            d = Document(r['AB'])
+            print r['EDAT'], [w[1] for w in d.keywords(top=6)]
 
-    # let PubMed handle keys
-    r['_id'] = int(r['PMID'])
-
-    if 'MH' in r:
-        for edge in combinations(r['MH'], 2):
-            if g.get_edge_data(*edge):
-                w = g.get_edge_data(*edge)['w'] + 1
-            else:
-                w =1
-            g.add_edge(*edge, w=w)
-
-
-
-for e in g.edges():
-    w = g.get_edge_data(*e)['w']
-
-    print "\t".join([e[0],e[1],str(w)])
