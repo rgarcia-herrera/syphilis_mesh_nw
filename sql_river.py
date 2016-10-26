bc9e40e1751aa89d4a10b66e576efe3d9d905007
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-10-26 16:38:21 -0500

    better formatting

--- sql_river.py
+++ sql_river.py
@@ -1,29 +1,33 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine, ForeignKey, Date, Text
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, Float, \
+    Boolean, create_engine, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class Course(Base):
     __tablename__ = 'courses'
-    id       = Column(Integer, primary_key=True)
-    label    = Column(String(200), nullable=True)
+    id = Column(Integer, primary_key=True)
+    label = Column(String(200), nullable=True)
     length = Column(Float, default=0)
     drains = relationship("Drain", back_populates="course")
 
     # first drain
     def source(self):
         # add not-artificial to query
-        return session.query(Drain).with_parent(self).order_by(Drain.offset).first()
+        return session.query(
+            Drain).with_parent(self).order_by(Drain.offset).first()
 
     # last drain
     def sink(self):
         # add not-artificial to query
-        return session.query(Drain).with_parent(self).order_by(Drain.offset.desc()).first()
+        return session.query(
+            Drain).with_parent(self).order_by(Drain.offset.desc()).first()
 
     def update_length(self):
-        if session.query(Drain).with_parent(self).order_by(Drain.offset).count()>=2:
+        if session.query(
+                Drain).with_parent(self).order_by(Drain.offset).count() >= 2:
             self.length = self.sink().offset - self.source().offset
 
     def add_drain(self, drain):
@@ -32,71 +36,69 @@ class Course(Base):
 
     def __repr__(self):
         self.update_length()
-        return "<course %s len=%s drains=%s>" % (self.label, self.length, len(self.drains))
+        return "<course %s len=%s drains=%s>" % (self.label,
+                                                 self.length,
+                                                 len(self.drains))
 
     def get_max_width(self):
-        return sorted(self.drains, key=lambda x: x.width, reverse=True)[0].width
+        return sorted(self.drains, key=lambda x: x.width,
+                      reverse=True)[0].width
 
-    
 
 class Drain(Base):
     __tablename__ = 'drains'
-    id       = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     width = Column(Float, default=0)
     offset = Column(Float, default=0)
 
     # artificial drains are automatically created to match courses
     artificial = Column(Boolean, default=False)
-    course_id  = Column(Integer, ForeignKey('courses.id'))
-    course     = relationship("Course",
-                              back_populates="drains")
+    course_id = Column(Integer, ForeignKey('courses.id'))
+    course = relationship("Course",
+                          back_populates="drains")
 
     def __repr__(self):
         if self.artificial:
-            a=' a'
+            a = ' a'
         else:
-            a=''
+            a = ''
         return "<drain %s o=%s w=%s%s>" % (self.id, self.offset, self.width, a)
-
 
 
 class River():
     def __init__(self, courses=[], init=True):
-        
         # auto init courses
         if init:
-            self.courses = session.query(Course).order_by(Course.id.desc()).all()
+            self.courses = session.query(
+                Course).order_by(Course.id.desc()).all()
         else:
-            self.courses=courses
+            self.courses = courses
 
     def match_drains(self):
         """ create artificial drains on courses
         so that all courses have all drains,
         that they may be aligned """
-        
+
         natural_drains = []
         for c in self.courses:
-            natural_drains += filter(lambda d: d.artificial==False, c.drains)
+            natural_drains += filter(lambda d: d.artificial is False, c.drains)
 
         for d in natural_drains:
             for c in self.courses:
                 if d not in c.drains:
-                    c.add_drain( Drain( offset=d.offset, artificial=True) )
-
-
+                    c.add_drain(Drain(offset=d.offset, artificial=True))
 
     def get_width(self):
         w = 0
         for course in self.courses:
             w += course.get_max_width()
         return w
-    
+
 # on assembling the river:
 # longest course should be centered
 # flank it with decreasing order length
 # horizontal center everything querying max width of river
-#class River(Base):
-#    pass
+
 
 
 
@@ -110,19 +112,18 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
-c=Course(label='volga',
-         drains=[ Drain(offset=3, width=6),
-                  Drain(offset=10, width=4)])
+c = Course(label='volga',
+           drains=[Drain(offset=3, width=6),
+                   Drain(offset=10, width=4)])
 session.add(c)
 
 
-c1=Course(label='nile',
-         drains=[ Drain(offset=1, width=6),
-                  Drain(offset=11, width=4),
-                  Drain(offset=20, width=7)])
+c1 = Course(label='nile',
+            drains=[Drain(offset=1, width=6),
+                    Drain(offset=11, width=4),
+                    Drain(offset=20, width=7)])
 
 session.add(c1)
 session.commit()
 
 river = River()
-
