4f020a3e1458e9de50884ce5af1d527e6489bdb2
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-10-30 18:37:21 -0600

    load keyword timeline, try to plot it

--- try_sql_river.py
+++ try_sql_river.py
@@ -1,36 +1,46 @@
 import svgwrite
 import sql_river as plt
 import random
+import csv
 
-for n in range(7):
-    c = plt.Course(label=str(n), fill =
-                   random.choice(['red','green','blue','orange',
-                                  'aliceblue', 'aqua', 'aquamarine', 'azure',
-                                  'bisque', 'blanchedalmond', 'blueviolet', 'brown',
-                                  'burlywood', 'cadetblue', 'chartreuse',
-                                  'chocolate', 'coral', 'cornflowerblue', 'crimson',
-                                  'darkblue', 'darkcyan', 'darkgoldenrod',
-                                  'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki',
-                                  'darkmagenta', 'darkolivegreen', 'darkorange',
-                                  'darkorchid', 'darkred', 'darksalmon',
-                                  'darkseagreen', 'darkslateblue', 'darkslategray',
-                                  'darkslategrey', 'darkturquoise', 'darkviolet',
-                                  'deeppink', 'deepskyblue', 'dimgray', 'dimgrey',
-                                  'dodgerblue', 'firebrick', 'forestgreen',
-                                  'gainsboro', 'gold', 'goldenrod', 'hotpink',
-                                  'indianred', 'indigo', 'khaki', 'lavender', ]))
+r = csv.DictReader(open('top_kw.csv'))
+
+def random_color():
+    return random.choice(['red','green','blue','orange', 'aliceblue',
+                          'aqua', 'aquamarine', 'azure', 'bisque',
+                          'blanchedalmond', 'blueviolet', 'brown',
+                          'burlywood', 'cadetblue', 'chartreuse',
+                          'chocolate', 'coral', 'cornflowerblue',
+                          'crimson', 'darkblue', 'darkcyan',
+                          'darkgoldenrod', 'darkgray', 'darkgreen',
+                          'darkgrey', 'darkkhaki', 'darkmagenta',
+                          'darkolivegreen', 'darkorange',
+                          'darkorchid', 'darkred', 'darksalmon',
+                          'darkseagreen', 'darkslateblue',
+                          'darkslategray', 'darkslategrey',
+                          'darkturquoise', 'darkviolet', 'deeppink',
+                          'deepskyblue', 'dimgray', 'dimgrey',
+                          'dodgerblue', 'firebrick', 'forestgreen',
+                          'gainsboro', 'gold', 'goldenrod', 'hotpink',
+                          'indianred', 'indigo', 'khaki', 'lavender',])
+
+for row in r:
+    kw = row.pop('kw')
+    c = plt.Course(label=kw,
+                   fill = random_color())
     plt.session.add(c)
     plt.session.commit()
-    o = 1
-    for m in range(random.randint(8,35)):
-        o += random.randint(80,100)
-        d = plt.Drain(offset=o,
-                      width=random.randint(25,60))
-        c.add_drain(d)
+    y = 1
+    for year in sorted(row.keys()):
+        width = float(row[year])
+        if width > 0:
+            d = plt.Drain(offset=y,
+                          width=width)
+            c.add_drain(d)
+        y += 100
 
 
-dwg = svgwrite.Drawing(filename='prueba.svg')
-
+dwg = svgwrite.Drawing(filename='syphilis_top_kw.svg')
 river = plt.River(dwg)
 river.centralize_current()
 river.to_svg()
