acd3f73038ce8e0f5a82b9b2407b779564b7ddaa
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-11-13 13:32:43 -0600

    plot given csv

--- try_sql_river.py
+++ try_sql_river.py
@@ -2,11 +2,22 @@ import svgwrite
 import sql_river as plt
 import random
 import csv
+import argparse
+import sys
 
-r = csv.DictReader(open('top_kw.csv'))
+parser = argparse.ArgumentParser(
+    description='Creates riverplot of timeline in csv.')
+parser.add_argument('csvfile',
+                    type=argparse.FileType('r'),
+                    default=sys.stdin, help="timeline in csv format")
+
+args = parser.parse_args()
+
+r = csv.DictReader(args.csvfile)
+
 
 def random_color():
-    return random.choice(['red','green','blue','orange', 'aliceblue',
+    return random.choice(['red', 'green', 'blue', 'orange', 'aliceblue',
                           'aqua', 'aquamarine', 'azure', 'bisque',
                           'blanchedalmond', 'blueviolet', 'brown',
                           'burlywood', 'cadetblue', 'chartreuse',
@@ -22,12 +33,12 @@ def random_color():
                           'deepskyblue', 'dimgray', 'dimgrey',
                           'dodgerblue', 'firebrick', 'forestgreen',
                           'gainsboro', 'gold', 'goldenrod', 'hotpink',
-                          'indianred', 'indigo', 'khaki', 'lavender',])
+                          'indianred', 'indigo', 'khaki', 'lavender'])
 
 for row in r:
     kw = row.pop('kw')
     c = plt.Course(label=kw,
-                   fill = random_color())
+                   fill=random_color())
     plt.session.add(c)
     plt.session.commit()
     y = 1
@@ -37,11 +48,13 @@ for row in r:
             d = plt.Drain(offset=y,
                           width=width)
             c.add_drain(d)
-        y += 100
+            print year, y, kw, width
+        y += 50
 
 
-dwg = svgwrite.Drawing(filename='syphilis_top_kw.svg')
+dwg = svgwrite.Drawing(filename="%s.svg" % args.csvfile.name.replace('.csv',''))
 river = plt.River(dwg)
 river.centralize_current()
+print "rendering"
 river.to_svg()
 dwg.save()
