b3c3a7813d59f848b39104d5d7dae5b7c88a37a5
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-11-14 23:05:24 -0600

    pandas style write out csv

--- test_pd_river.py
+++ test_pd_river.py
@@ -2,10 +2,10 @@ import pandas as pd
 from pd_river import Course, Drain, River
 import random
 
-timeline_df = pd.read_csv('smalfix_alt.csv',
+timeline_df = pd.read_csv('sampled_top40.csv',
                           parse_dates=['year'], index_col='year')
 
-r = River('aguas.svg', timeline_df)
+r = River('sampled_top40.svg', timeline_df)
 
 for c in r.courses:
     print c, c.get_length(), sorted(c.drains, key=lambda x: x.offset)
--- top_keywords_per_year.py
+++ top_keywords_per_year.py
@@ -6,14 +6,22 @@ from Bio import Medline
 from math import log
 from pprint import pprint
 import csv
+import datetime
+import pandas as pd
+import numpy as np
+import random
 
 records = Medline.parse(open('syphilis_pubmed.medline'))
-# records = Medline.parse(open('fixture.medline'))
+#records = Medline.parse(open('fixture.medline'))
 
 terms = {n: Year(n) for n in range(1817, 2017)}
 all_kw = set()
 
 for r in records:
+    # discard 0.9
+    if random.random() > 0.1:
+        continue
+    
     c = Citation(r)
     kw = c.get_meshterms()
     if len(kw.keys()) == 0:
@@ -21,56 +29,56 @@ for r in records:
 
     terms[c.date.year].add_ref(r.get('PMID'), kw)
 
-    # add them to global kw set
-    for w in kw:
-        all_kw.add(w)
+all_kw = set.union(*[terms[y].get_keywords() for y in range(1817, 2017)])
 
-normalized_terms = dict()
-for year in terms:
-    normalized_terms[year] = terms[year].get_normalized_kw_fq()
+all_kw = sorted(list(all_kw))
+
+data = []
+for year in range(1817,2017):
+    nt = terms[year].get_normalized_kw_fq()
+    row = [np.log((nt.get(t,0) * len(terms[year].refs))+1)*50 for t in all_kw]
+    data.append(row)
+
+index = pd.date_range(start="1817", end="2016", freq='365D')
+df = pd.DataFrame(data, index=index, columns=all_kw)
 
 
+top = df.astype(bool).sum(axis=0).sort_values(ascending=False)
+top40 = [kw for kw in top.index[:40]]
+
+df[top40].to_csv('sampled_top40.csv', sep=',', encoding='utf-8', index_label='year')
+
+
+    
 # write csv file of keywords and their usage
-with open('syphilis_all_kw.csv', 'w') as csvfile:
-    w = csv.writer(csvfile)
-    w.writerow(['year', ] + sorted(all_kw))
-    for year in range(1817, 2017):
-        row = [year, ]
-        for kw in sorted(all_kw):
-            width = normalized_terms[year].get(kw, 0) \
-                    * len(terms[year].refs.keys()) \
-                    + 3
+# with open('syphilis_all_kw.csv', 'w') as csvfile:
+#     w = csv.writer(csvfile)
+#     w.writerow(['year', ] + sorted(all_kw))
+#     for year in range(1817, 2017):
+#         row = [year, ]
+#         for kw in sorted(all_kw):
+#             width = normalized_terms[year].get(kw, 0) \
+#                     * len(terms[year].refs.keys()) \
+#                     + 3
 
-            if width > 3:
-                row.append("%.2f" % log(width))
-            else:
-                row.append(0)
-        w.writerow(row)
+#             if width > 3:
+#                 row.append("%.2f" % log(width))
+#             else:
+#                 row.append(0)
+#         w.writerow(row)
 
 
-# top_terms = {n: {'kw': [], 'publications': 0} for n in range(1817, 2017)}
-# for year in terms:
-#     top_terms[year]['publications'] = terms[year]['publications']
-#     if top_terms[year]['publications']/20 <= 10:
-#         top = 10
-#     else:
-#         top = top_terms[year]['publications']/20   # top 5%
-
-#     d = Document(terms[year]['kw'])
-#     kwords = d.keywords(top=top)
-
-#     top_terms[year]['kw'] = {kw[1]:kw[0] for kw in kwords}
 
 
 #exit()
 
 # compute distances among all pairs of keywords
-sdist = {}
-for pair in combinations(all_kw, 2):
-    sdist[pair] = (distance.levenshtein(*pair),
-                   distance.jaccard(*pair))
+# sdist = {}
+# for pair in combinations(all_kw, 2):
+#     sdist[pair] = (distance.levenshtein(*pair),
+#                    distance.jaccard(*pair))
 
-# which pairs are really close?
-for pair in sdist:
-    if sdist[pair][0] < 5 and sdist[pair][1] < 0.3:
-        print pair, sdist[pair]
+# # which pairs are really close?
+# for pair in sdist:
+#     if sdist[pair][0] < 5 and sdist[pair][1] < 0.3:
+#         print pair, sdist[pair]
