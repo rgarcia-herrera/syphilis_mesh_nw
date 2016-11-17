81acfaead64a86954ecd74062aee2bc4f2389836
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-11-16 19:20:48 -0600

    argparse interface too

--- test_pd_river.py
+++ test_pd_river.py
@@ -1,25 +1,27 @@
 import pandas as pd
-from pd_river import Course, Drain, River
-import random
+from pd_river import River
+import argparse
 
-timeline_df = pd.read_csv('sampled_top40.csv',
+parser = argparse.ArgumentParser(description='Create riverplot from csv timeline')
+parser.add_argument('--csv',
+                    type=argparse.FileType('r'),
+                    required=True,
+                    help="csv timeline")
+parser.add_argument('--output',
+                    type=argparse.FileType('w'),
+                    required=True,
+                    help="path to output svg")
+
+args = parser.parse_args()
+
+timeline_df = pd.read_csv(args.csv,
                           parse_dates=['year'], index_col='year')
 
-r = River('sampled_top40.svg', timeline_df)
-
-# for c in r.courses:
-#     print c, c.get_length(), sorted(c.drains, key=lambda x: x.offset)
-
-# print "longest", r.get_longest_course()
+out_path = args.output.name
+args.output.close()
+r = River(out_path, timeline_df)
 
 print "rendering"
 
 r.to_svg()
 r.dwg.save()
-
-
-# by_len = timeline_df.astype(bool).sum(axis=0)
-# by_len.sort_values(ascending=False, inplace=True)
-
-# for i, row in by_len.iteritems():
-#     print i
