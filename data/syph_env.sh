71872077c79d409f7615b81f3c9ae4d4d50f1038
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-12-11 12:49:21 -0600

    wrapper script activates virtualenv

--- data/syph_env.sh
+++ data/syph_env.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+
+
 source /srv/home/rgarcia/python_envs/syphilis/bin/activate
 
-echo "creating pickle"
-python ../pubmed_timeline/bin/pubmed_graph_for_year.py \
-	     --start_year $1 \
-	     --thru_year $2 \
-	     --mode $3 \
-	     --ignore syphilis_ignore.txt \
-	     --top $4 \
-	     --pickle $5 \
-	     --medline syphilis_pubmed.medline \
+`$@`
+
+# echo "creating pickle"
+# python ../pubmed_timeline/bin/pubmed_graph_for_year.py \
+# 	     --start_year $1 \
+# 	     --thru_year $2 \
+# 	     --mode $3 \
+# 	     --top $4 \
+# 	     --ignore syphilis_ignore.txt \
+# 	     --groups syphilis_groups.json \
+# 	     --pickle $5 \
+# 	     --medline syphilis_pubmed.medline
 
 
-echo "plotting pickle"	  
-plot_nx_graph.py --pickle $5 --svg $6
+# echo "plotting pickle"
+# plot_nx_graph.py --pickle $5 --svg $6
