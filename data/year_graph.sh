8308c435d10c688dd79b63e33cbfb57fcecacf58
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-12-10 23:21:36 -0600

    adapted for patung

--- data/year_graph.sh
+++ data/year_graph.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-source ~/.virtualenvs/nb/bin/activate
+source /srv/home/rgarcia/python_envs/syphilis/bin/activate
 
-xzcat syphilis_pubmed.medline.xz \
-    | python ../pubmed_timeline/bin/pubmed_graph_for_year.py \
+echo "creating pickle"
+python ../pubmed_timeline/bin/pubmed_graph_for_year.py \
 	     --start_year $1 \
 	     --thru_year $2 \
 	     --mode $3 \
 	     --ignore syphilis_ignore.txt \
 	     --top $4 \
-	     --pickle $5
+	     --pickle $5 \
+	     --medline syphilis_pubmed.medline \
 
+
+echo "plotting pickle"	  
 plot_nx_graph.py --pickle $5 --svg $6
