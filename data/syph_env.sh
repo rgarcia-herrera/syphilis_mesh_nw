e0d38fed9f4eeb63ee6ce599b572f58f4f583c89
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-12-11 14:42:51 -0600

    just activate env then run

--- data/syph_env.sh
+++ data/syph_env.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-
-
 source /srv/home/rgarcia/python_envs/syphilis/bin/activate
 
-`$@`
+$@
 
-# echo "creating pickle"
-# python ../pubmed_timeline/bin/pubmed_graph_for_year.py \
-# 	     --start_year $1 \
-# 	     --thru_year $2 \
-# 	     --mode $3 \
-# 	     --top $4 \
-# 	     --ignore syphilis_ignore.txt \
-# 	     --groups syphilis_groups.json \
-# 	     --pickle $5 \
-# 	     --medline syphilis_pubmed.medline
-
-
-# echo "plotting pickle"
-# plot_nx_graph.py --pickle $5 --svg $6
