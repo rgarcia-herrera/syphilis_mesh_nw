0d602b823d22c3e4154607c1080e373004e966d0
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-12-11 14:43:45 -0600

    parallel prune

---
+++ data/condor_prune.py
@@ -0,0 +1,22 @@
+from sh import find
+
+print "executable = syph_env.sh"
+
+plantilla = """
+arguments = ./prune_graph.py \
+             --pickle {pickle} \
+             --out {out}
+
+log          = condor_log/{run_id}.log                                                    
+output       = condor_log/{run_id}.out
+error        = condor_log/{run_id}.err
+queue
+"""
+
+for p in find('pickles', '-iname', '*pickle'):
+    inpath = p.strip()
+    outpath = inpath.replace('.pickle', '_pruned.pickle')
+    run_id = "prune_%s" % inpath.replace('.pickle','')
+    print plantilla.format(pickle=inpath,
+                           out=outpath,
+                           run_id=run_id)
