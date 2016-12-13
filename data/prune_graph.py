729826156a8a3fca743a243dfd89d969fd8e4c7f
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-12-13 16:38:17 -0600

    edge and node weight thresholds

--- data/prune_graph.py
+++ data/prune_graph.py
@@ -11,6 +11,17 @@ parser.add_argument('--pickle',
                     type=argparse.FileType('r'),
                     required=True,
                     help="path to pickled nx graph")
+
+parser.add_argument('--edgeth',
+                    type=int,
+                    default=0,
+                    help="keep edges with weights over this threshold")
+
+parser.add_argument('--nodeth',
+                    type=int,
+                    default=0,
+                    help="keep nodes with weights over this threshold")
+
 parser.add_argument('--out',
                     type=argparse.FileType('w'),
                     required=True,
@@ -20,11 +31,11 @@ args = parser.parse_args()
 g = pickle.load(args.pickle)
 
 for e in g.edges():
-    if g.get_edge_data(*e)['w'] <= 1:
+    if g.get_edge_data(*e)['w'] <= args.edgeth:
         g.remove_edge(*e)        
 
 for n in g.nodes():
-    if g.node[n]['w'] == 1 or len(g.neighbors(n)) < 1:
+    if g.node[n]['w'] == 1 or len(g.neighbors(n)) < args.nodeth:
         g.remove_node(n)
 
 pickle.dump(g, args.out)
